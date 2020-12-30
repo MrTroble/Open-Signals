5cdf8e4b4d7acd0e88a605debfba398f31f6d22d
FGBxRamel <kretschmer.raziel@gmail.com>
2020-12-30 17:01:33 +0100

    Tried making Blockstates for HP Signals

---
+++ src/main/java/net/gir/girsignals/blocks/HVSignal.java
@@ -0,0 +1,59 @@
+package net.gir.girsignals.blocks;
+
+import ibxm.Player;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Rotations;
+import net.minecraft.world.World;
+
+public class HVSignal extends Block {
+
+	public HVSignal() {
+		super(Material.ROCK);
+		setDefaultState(getDefaultState().withProperty(FACING, EnumFacing.NORTH));
+	}
+
+	public static final PropertyDirection FACING = BlockHorizontal.FACING;
+
+	@Override
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+			float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+		return getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(FACING).getHorizontalIndex();
+	}
+
+	@Override
+	public IBlockState withRotation(IBlockState state, Rotation rot) {
+		return state.withRotation(rot);
+	}
+
+	@Override
+	public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+		return state.withMirror(mirrorIn);
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(HVSignal.this, FACING);
+	}
+
+}
