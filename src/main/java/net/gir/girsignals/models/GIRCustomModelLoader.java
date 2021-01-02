b924ae997c32b7918d9f7feb34b6583b25fd58c8
MrTroble <nifroh56@gmail.com>
2021-01-02 02:28:49 +0100

    Added off lamp overlayes

--- src/main/java/net/gir/girsignals/models/GIRCustomModelLoader.java
+++ src/main/java/net/gir/girsignals/models/GIRCustomModelLoader.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.function.Consumer;
 
 import net.gir.girsignals.GirsignalsMain;
-import net.gir.girsignals.blocks.SignalHV;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.EnumFacing;
@@ -28,13 +27,46 @@ public class GIRCustomModelLoader implements ICustomModelLoader {
 			cm.register("hv_mast_number", ebs -> true, 2);
 			cm.register("hv_zs3v", ebs -> true, 3);
 			cm.register("hv_vr", ebs -> true, 4);
-			cm.register("hv_zs1", ebs -> ebs.getValue(SignalHV.ZS1) != null, 5f);
+			cm.register("hv_zs1", ebs -> false, 4.4f);
 			cm.register("hv_zs7", ebs -> true, 4.6f);
-			cm.register("hv_hp", ebs -> {
-				System.out.println(ebs.toString());
-				return ebs.getValue(SignalHV.HAUPTSIGNAL) != null;
-			}, 5.4f);
+			cm.register("hv_hp", ebs -> true, 5.4f);
 			cm.register("hv_zs3", ebs -> true, 6.9f);
+			
+			// HP 2
+			cm.register("lamp_black", ebs -> true, (3.5f/32.0f), 5 - (1/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			// HP 0
+			cm.register("lamp_black", ebs -> true, (3.5f/32.0f), 5 + (23/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black", ebs -> true, -(6.5f/32.0f), 5 + (23/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			// HP 1/2 (green)
+			cm.register("lamp_black", ebs -> true, (3.5f/32.0f), 6 + (1/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// VR0
+			cm.register("lamp_black", ebs -> true, (10.5f/32.0f), 3 + (12.5f/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black", ebs -> true, -(5.5f/32.0f), 3 + (30.5f/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// VR1
+			cm.register("lamp_black", ebs -> true, (2.5f/32.0f), 3 + (12.5f/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black", ebs -> true, -(13.5f/32.0f), 3 + (30.5f/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// RS
+			cm.register("lamp_black_small", ebs -> true, -(6.5f/32.0f), 5 + (15/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black_small", ebs -> true, (3.5f/32.0f), 5 + (15/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// Status light
+			cm.register("lamp_black_small", ebs -> true, (3.5f/32.0f), 5 + (7/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// ZS 1
+			cm.register("lamp_black_small", ebs -> false, -(1.5f/32.0f), 4 + (21/32.0f), -((8/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black_small", ebs -> false, -(4.5f/32.0f), 4 + (15.3f/32.0f), -((8/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black_small", ebs -> false, (1.5f/32.0f), 4 + (15.3f/32.0f), -((8/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// VR Short distance lamp
+			cm.register("lamp_black_small", ebs -> true, (8.5f/32.0f), 3 + (30.5f/32.0f), -((6/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			
+			// ZS 7
+			cm.register("lamp_black_small", ebs -> true, -(1.5f/32.0f), 4 + (15.5f/32.0f), -((8/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black_small", ebs -> true, -(4.5f/32.0f), 4 + (21.5f/32.0f), -((8/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
+			cm.register("lamp_black_small", ebs -> true, (1.5f/32.0f), 4 + (21.5f/32.0f), -((8/32.0f) + 0.01f), 0.1f, 0.1f, 0f);
 		});
 	}
 
--- src/main/java/net/gir/girsignals/models/SignalCustomModel.java
+++ src/main/java/net/gir/girsignals/models/SignalCustomModel.java
@@ -26,11 +26,14 @@ import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.common.model.TRSRTransformation;
 import net.minecraftforge.common.property.IExtendedBlockState;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class SignalCustomModel implements IModel {
 
 	private ArrayList<ResourceLocation> textures = new ArrayList<>();
-	private HashMap<Predicate<IExtendedBlockState>, Pair<IModel, Vector3f>> modelCache = new HashMap<>();
+	private HashMap<Predicate<IExtendedBlockState>, Pair<IModel, Pair<Vector3f, Vector3f>>> modelCache = new HashMap<>();
 	private IBakedModel cachedModel = null;
 	private EnumFacing facing = EnumFacing.NORTH;
 	
@@ -51,10 +54,15 @@ public class SignalCustomModel implements IModel {
 						if(trs.isPresent()) {
 							TRSRTransformation trsr = trs.get();
 							ItemTransformVec3f itf = trsr.toItemTransform();
-							Vector3f vec = m.second();
-							itf.translation.x += vec.x;
+							Vector3f vec = m.second().first();
+							itf.translation.x += (facing != EnumFacing.SOUTH && facing != EnumFacing.NORTH) ? ((facing == EnumFacing.WEST ? 1:-1)*vec.z) : ((facing == EnumFacing.NORTH ? 1:-1)* vec.x);
 							itf.translation.y += vec.y;
-							itf.translation.z += vec.z;
+							itf.translation.z += (facing != EnumFacing.SOUTH && facing != EnumFacing.NORTH) ? ((facing == EnumFacing.WEST ? -1:1)*vec.x) : ((facing == EnumFacing.NORTH ? 1:-1)* vec.z);
+
+							Vector3f scale = m.second().second();
+							itf.scale.x += scale.x;
+							itf.scale.y += scale.y;
+							itf.scale.z += scale.z;
 							if(facing != EnumFacing.NORTH)
 								itf.rotation.y += facing.getHorizontalAngle();
 							trsr = TRSRTransformation.from(itf);
@@ -84,9 +92,13 @@ public class SignalCustomModel implements IModel {
 	}
 
 	protected void register(String name, Predicate<IExtendedBlockState> state, float x, float y, float z) {
+		this.register(name, state, x, y, z, 0, 0, 0);
+	}
+	
+	protected void register(String name, Predicate<IExtendedBlockState> state, float x, float y, float z, float xs, float ys, float zs) {
 		IModel m = ModelLoaderRegistry.getModelOrLogError(new ResourceLocation(GirsignalsMain.MODID, "block/" + name),
 				"Couldn't find " + name);
 		textures.addAll(m.getTextures());
-		modelCache.put(state, Pair.of(m, new Vector3f(x, y, z)));
+		modelCache.put(state, Pair.of(m, Pair.of(new Vector3f(x, y, z), new Vector3f(xs, ys, zs))));
 	}
 }
