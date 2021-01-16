77761b10add021b96ae483c5dcb249fd3e27f6d5
MrTroble <nifroh56@gmail.com>
2021-01-16 21:53:05 +0100

    Fixed issue fix

--- src/main/java/net/gir/girsignals/blocks/SignalBlock.java
+++ src/main/java/net/gir/girsignals/blocks/SignalBlock.java
@@ -58,7 +58,7 @@ public class SignalBlock extends Block implements ITileEntityProvider {
 	
 	public static ItemStack pickBlock(EntityPlayer player) {
 		// Compatibility issues with other mods ...
-		if(!Minecraft.getMinecraft().gameSettings.keyBindPickBlock.isPressed()) return ItemStack.EMPTY;
+		if(!Minecraft.getMinecraft().gameSettings.keyBindPickBlock.isKeyDown()) return ItemStack.EMPTY;
         for (int k = 0; k < InventoryPlayer.getHotbarSize(); ++k)
         {
         	if(player.inventory.getStackInSlot(k).getItem().equals(GIRItems.PLACEMENT_TOOL)) {
