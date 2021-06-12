f2e4acb9dc7485b7acedc2679d30ff02f332297b
MrTroble <nifroh56@gmail.com>
2021-06-12 12:55:13 +0200

    Saved container

--- src/main/java/eu/gir/girsignals/guis/ContainerSignalController.java
+++ src/main/java/eu/gir/girsignals/guis/ContainerSignalController.java
@@ -4,6 +4,7 @@ import eu.gir.girsignals.tileentitys.SignalControllerTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerFurnace;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -17,7 +18,7 @@ public class ContainerSignalController extends Container {
 	
 	@Override
 	public void detectAndSendChanges() {
-		
+		ContainerFurnace
 	}
 	
 	@SideOnly(Side.CLIENT)
