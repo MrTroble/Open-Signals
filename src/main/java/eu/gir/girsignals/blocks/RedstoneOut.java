901225012bc6523a9ce6ad83bddd388fa8828fe5
Philipp <philippdal@gmail.com>
2022-02-24 21:12:48 +0100

    Updated redstone io

--- src/main/java/eu/gir/girsignals/blocks/RedstoneOut.java
+++ src/main/java/eu/gir/girsignals/blocks/RedstoneOut.java
@@ -3,11 +3,34 @@ package eu.gir.girsignals.blocks;
 import eu.gir.girsignals.init.GIRTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 
 public class RedstoneOut extends Block {
+	
+	public static final PropertyBool POWER = PropertyBool.create("power");
 
 	public RedstoneOut() {
 		super(Material.ROCK);
 		setCreativeTab(GIRTabs.tab);
+		this.setDefaultState(getDefaultState().withProperty(POWER, false));
 	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(POWER) ? 0:1;
+	}
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(POWER, meta == 1);
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { POWER });
+	}
+
 }
