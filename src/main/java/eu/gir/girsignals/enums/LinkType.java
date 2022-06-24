01806b174aded61eef8a7797a6703247439ce029
MrTroble <nifroh56@gmail.com>
2022-06-24 13:21:55 +0200

    ref: removed unused parameter in enums

--- src/main/java/eu/gir/girsignals/enums/LinkType.java
+++ src/main/java/eu/gir/girsignals/enums/LinkType.java
@@ -1,15 +1,8 @@
 package eu.gir.girsignals.enums;
 
-import eu.gir.girsignals.signalbox.entrys.PathEntryType;
-import net.minecraft.util.math.BlockPos;
-
 public enum LinkType {
-    SIGNAL(PathEntryType.SIGNAL), INPUT(PathEntryType.RESETING), OUTPUT(PathEntryType.OUTPUT);
-
-    private final PathEntryType<BlockPos> type;
-
-    private LinkType(final PathEntryType<BlockPos> type) {
-        this.type = type;
-    }
+	SIGNAL,
+	INPUT,
+	OUTPUT
 
 }
--- src/main/java/eu/gir/girsignals/enums/PathType.java
+++ src/main/java/eu/gir/girsignals/enums/PathType.java
@@ -2,36 +2,31 @@ package eu.gir.girsignals.enums;
 
 import java.util.Arrays;
 
-import eu.gir.girsignals.signalbox.entrys.PathEntryType;
-import net.minecraft.util.math.BlockPos;
-
 public enum PathType {
-
-    NORMAL(PathEntryType.SIGNAL, EnumGuiMode.HP),
-    SHUNTING(PathEntryType.SIGNAL, EnumGuiMode.RS, EnumGuiMode.RA10), NONE(PathEntryType.SIGNAL);
-
-    private final EnumGuiMode[] modes;
-    private final PathEntryType<BlockPos> type;
-
-    private PathType(final PathEntryType<BlockPos> type, final EnumGuiMode... modes) {
-        this.modes = modes;
-        this.type = type;
-    }
-
-    public boolean hasMode(final EnumGuiMode mode) {
-        return Arrays.stream(modes).anyMatch(mode::equals);
-    }
-
-    /**
-     * @return the modes
-     */
-    public EnumGuiMode[] getModes() {
-        return modes;
-    }
-
-    public static final PathType of(final EnumGuiMode mode) {
-        return Arrays.stream(PathType.values()).filter(type -> type.hasMode(mode)).findFirst()
-                .orElse(NONE);
-    }
-
+	
+	NORMAL(EnumGuiMode.HP),
+	SHUNTING(EnumGuiMode.RS, EnumGuiMode.RA10),
+	NONE();
+	
+	private final EnumGuiMode[] modes;
+	
+	private PathType(final EnumGuiMode... modes) {
+		this.modes = modes;
+	}
+	
+	public boolean hasMode(final EnumGuiMode mode) {
+		return Arrays.stream(modes).anyMatch(mode::equals);
+	}
+	
+	/**
+	 * @return the modes
+	 */
+	public EnumGuiMode[] getModes() {
+		return modes;
+	}
+	
+	public static final PathType of(final EnumGuiMode mode) {
+		return Arrays.stream(PathType.values()).filter(type -> type.hasMode(mode)).findFirst().orElse(NONE);
+	}
+	
 }
