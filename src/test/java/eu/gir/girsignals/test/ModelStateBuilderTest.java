b1b594f6ddf2b60906659dcac53af3655450f317
Uhutown <cedric.hathaway05@gmail.com>
2022-09-14 17:00:56 +0200

    removed unused code

--- src/test/java/eu/gir/girsignals/test/ModelStateBuilderTest.java
+++ src/test/java/eu/gir/girsignals/test/ModelStateBuilderTest.java
@@ -1,19 +1,12 @@
 package eu.gir.girsignals.test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 import eu.gir.girsignals.GIRFileReader;
-import eu.gir.girsignals.blocks.Signal;
-import eu.gir.girsignals.blocks.signals.SignalHV;
-import eu.gir.girsignals.models.GIRBlockstateParser;
 import eu.gir.girsignals.models.ModelStats;
 import eu.gir.girsignals.models.TextureStats;
-import net.minecraftforge.common.property.IUnlistedProperty;
 
 public class ModelStateBuilderTest {
 
@@ -21,6 +14,7 @@ public class ModelStateBuilderTest {
     public void testModelstats() {
         System.out.println(
                 GIRFileReader.readallFilesfromDierectory("/assets/girsignals/modeldefinitions"));
+        
         final Map<String, ModelStats> map = ModelStats
                 .getfromJson("/assets/girsignals/modeldefinitions");
         map.forEach((test1, test2) -> {
@@ -42,36 +36,5 @@ public class ModelStateBuilderTest {
                 }
             });
         });
-        final Field[] fields = SignalHV.class.getDeclaredFields();
-        for (final Field field : fields) {
-            //System.out.println(field);
-        }
-    }
-
-    @SuppressWarnings("rawtypes")
-    public void testProperties() {
-        ArrayList<IUnlistedProperty> signalProperties = new ArrayList<>();
-        signalProperties.clear();
-        if (!this.getClass().equals(Signal.class)) {
-            for (final Field f : SignalHV.class.getDeclaredFields()) {
-                final int mods = f.getModifiers();
-                if (Modifier.isFinal(mods) && Modifier.isStatic(mods) && Modifier.isPublic(mods)) {
-                    try {
-                        signalProperties.add((IUnlistedProperty) f.get(null));
-                    } catch (final IllegalArgumentException | IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        signalProperties.forEach(property -> {
-            System.out.println(property.toString());
-        });
-    }
-
-    public void testBlockstateParser() {
-        final Object state = GIRBlockstateParser.getPredicateFromString(
-                "with(STOPSIGNAL, HP0) && (with(HPTYPE, STOPSIGNAL) || !has(HPTYPE))", null, null);
-        System.out.println(state);
     }
 }
