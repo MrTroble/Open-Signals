475925c2d972471491abc87006f939361c6bc92d
Uhutown <cedric.hathaway05@gmail.com>
2022-10-10 16:36:50 +0200

    removed unused code

--- src/main/java/eu/gir/girsignals/models/TextureStats.java
+++ src/main/java/eu/gir/girsignals/models/TextureStats.java
@@ -7,94 +7,89 @@ import com.google.common.collect.ImmutableMap;
 
 public class TextureStats {
 
-    private float x = 0;
-    private float y = 0;
-    private float z = 0;
+	private float x = 0;
+	private float y = 0;
+	private float z = 0;
 
-    private boolean loadOFFstate = true;
-    private String blockstate;
-    private Map<String, String> retexture;
-    private Map<String, Map<String, String>> extentions;
+	private boolean loadOFFstate = true;
+	private String blockstate;
+	private Map<String, String> retexture;
+	private Map<String, Map<String, String>> extentions;
 
-    public float getOffsetX() {
-        return x;
-    }
+	public float getOffsetX() {
+		return x;
+	}
 
-    public float getOffsetY() {
-        return y;
-    }
+	public float getOffsetY() {
+		return y;
+	}
 
-    public float getOffsetZ() {
-        return z;
-    }
+	public float getOffsetZ() {
+		return z;
+	}
 
-    public boolean loadOFFstate() {
-        return loadOFFstate;
-    }
+	public boolean loadOFFstate() {
+		return loadOFFstate;
+	}
 
-    public boolean isautoBlockstate() {
-        return blockstate == null;
-    }
+	public boolean isautoBlockstate() {
+		return blockstate == null;
+	}
 
-    public String getBlockstate() {
-        return blockstate;
-    }
+	public String getBlockstate() {
+		return blockstate;
+	}
 
-    public void resetStates(final String state, final Map<String, String> retexture) {
+	public void resetStates(final String state, final Map<String, String> retexture) {
 
-        blockstate = state;
+		blockstate = state;
 
-        this.retexture = retexture;
-    }
+		this.retexture = retexture;
+	}
 
-    public ImmutableMap<String, String> getRetextures() {
+	public ImmutableMap<String, String> getRetextures() {
 
-        if (retexture == null) 
-            retexture = new HashMap<>();
-            
-        return ImmutableMap.copyOf(retexture);
-    }
+		if (retexture == null)
+			retexture = new HashMap<>();
 
-    /**
-     * 1. String: Name of the file to get load 2. String: SEProperty 3. String: The
-     * key of the retexture map
-     */
-    public ImmutableMap<String, Map<String, String>> getExtentions() {
-        
-        if (extentions == null) {
-            extentions = new HashMap<>();
-        }
-        return ImmutableMap.copyOf(extentions);
-    }
+		return ImmutableMap.copyOf(retexture);
+	}
 
-    /**
-     * @return true if this model with these parameters should/can be loaded
-     */
+	/**
+	 * 1. String: Name of the file to get load 2. String: SEProperty 3. String: The
+	 * key of the retexture map
+	 */
+	public ImmutableMap<String, Map<String, String>> getExtentions() {
 
-    public boolean appendExtention(final String seprop, final String enums,
-            final String retexturekey, final String retexureval) {
+		if (extentions == null) {
+			extentions = new HashMap<>();
+		}
+		return ImmutableMap.copyOf(extentions);
+	}
 
-        retexture = new HashMap<>();
-        
-        if (!loadOFFstate && enums.equalsIgnoreCase("OFF"))
-            return false;
+	/**
+	 * @return true if this model with these parameters should/can be loaded
+	 */
 
-        if (this.retexture == null) {
+	public boolean appendExtention(final String seprop, final String enums, final String retexturekey,
+			final String retexureval) {
 
-            this.retexture = new HashMap<>();
-        }
+		retexture = new HashMap<>();
 
-        this.retexture.put(retexturekey, retexureval);
+		if (!loadOFFstate && enums.equalsIgnoreCase("OFF"))
+			return false;
 
-        if (this.blockstate.length() < 4) {
+		this.retexture.put(retexturekey, retexureval);
 
-            this.blockstate = "with(" + seprop + "." + enums + ")";
+		if (this.blockstate.length() < 4) {
 
-        } else {
+			this.blockstate = "with(" + seprop + "." + enums + ")";
 
-            this.blockstate += " && with(" + seprop + "." + enums + ")";
-        }
+		} else {
 
-        return true;
-    }
+			this.blockstate += " && with(" + seprop + "." + enums + ")";
+		}
+
+		return true;
+	}
 }
