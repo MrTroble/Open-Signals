4e9c1a2883626df207e75e8f33bfc1ee69006926
Uhutown <cedric.hathaway05@gmail.com>
2022-10-17 21:41:29 +0200

    fix: Sema Config

--- src/main/java/eu/gir/girsignals/signalbox/config/SemaphoreConfig.java
+++ src/main/java/eu/gir/girsignals/signalbox/config/SemaphoreConfig.java
@@ -3,12 +3,15 @@ package eu.gir.girsignals.signalbox.config;
 import java.util.HashMap;
 import java.util.Optional;
 
+import eu.gir.girsignals.EnumSignals.HLExit;
+import eu.gir.girsignals.EnumSignals.HLLightbar;
 import eu.gir.girsignals.EnumSignals.HP;
 import eu.gir.girsignals.EnumSignals.HPBlock;
 import eu.gir.girsignals.EnumSignals.HPHome;
 import eu.gir.girsignals.EnumSignals.KS;
 import eu.gir.girsignals.EnumSignals.KSMain;
 import eu.gir.girsignals.EnumSignals.SemaDist;
+import eu.gir.girsignals.EnumSignals.SemaType;
 import eu.gir.girsignals.EnumSignals.ZS32;
 import eu.gir.girsignals.SEProperty;
 import eu.gir.girsignals.blocks.signals.SignalHL;
@@ -35,20 +38,6 @@ public final class SemaphoreConfig implements ISignalAutoconfig {
                 values.put(SignalSemaphore.SEMA_VR, SemaDist.VR2);
             }
         }
-    }
-
-    @SuppressWarnings("rawtypes")
-    private static void checkStop(final boolean stop, final HashMap<SEProperty, Object> values) {
-
-        if (stop) {
-
-            values.put(SignalSemaphore.SEMA_VR, SemaDist.VR0);
-
-        } else {
-
-            values.put(SignalSemaphore.SEMA_VR, SemaDist.VR1);
-        }
-
     }
 
     public static final SemaphoreConfig SEMAPHORE_CONFIG = new SemaphoreConfig();
@@ -101,7 +90,23 @@ public final class SemaphoreConfig implements ISignalAutoconfig {
                     .getProperty(SignalSemaphore.WING1);
 
             final Optional<Boolean> wing2 = (Optional<Boolean>) info.next
-                    .getProperty(SignalSemaphore.WING1);
+                    .getProperty(SignalSemaphore.WING2);
+
+            final Optional<HPHome> nextHPHOME = (Optional<HPHome>) info.next
+                    .getProperty(SignalHV.HPHOME);
+
+            final Optional<HLLightbar> getlightbar = (Optional<HLLightbar>) info.next
+                    .getProperty(SignalHL.LIGHTBAR);
+
+            final Optional<HP> nextHP = (Optional<HP>) info.next.getProperty(SignalHV.STOPSIGNAL);
+
+            final Optional<ZS32> ksZS3 = (Optional<ZS32>) info.next.getProperty(SignalKS.ZS3);
+
+            final Optional<KS> ksStopsignal = (Optional<KS>) info.next
+                    .getProperty(SignalKS.STOPSIGNAL);
+
+            final Optional<SemaType> semaType = (Optional<SemaType>) info.next
+                    .getProperty(SignalSemaphore.SEMATYPE);
 
             final boolean ksStop = info.next.getProperty(SignalKS.MAINSIGNAL)
                     .filter(KSMain.HP0::equals).isPresent()
@@ -110,31 +115,44 @@ public final class SemaphoreConfig implements ISignalAutoconfig {
 
             final boolean hvStop1 = info.next.getProperty(SignalHV.HPBLOCK)
                     .filter(HPBlock.HP0::equals).isPresent()
-                    || info.next.getProperty(SignalHV.HPHOME).filter(HPHome.HP0::equals).isPresent()
-                    || info.next.getProperty(SignalHV.HPHOME)
-                            .filter(HPHome.HP0_ALTERNATE_RED::equals).isPresent();
+                    || nextHPHOME.filter(HPHome.HP0::equals).isPresent()
+                    || nextHPHOME.filter(HPHome.HP0_ALTERNATE_RED::equals).isPresent();
 
-            final boolean hvStop2 = info.next.getProperty(SignalHV.STOPSIGNAL)
-                    .filter(HP.HP0::equals).isPresent()
-                    || info.next.getProperty(SignalHV.STOPSIGNAL).filter(HP.SHUNTING::equals)
-                            .isPresent();
+            final boolean hvStop2 = nextHP.filter(HP.HP0::equals).isPresent()
+                    || nextHP.filter(HP.SHUNTING::equals).isPresent();
 
             final boolean hlStop = info.next.getProperty(SignalHL.STOPSIGNAL)
                     .filter(a -> Signallists.HL_STOP.contains(a)).isPresent()
                     || info.next.getProperty(SignalHL.EXITSIGNAL)
                             .filter(d -> Signallists.HLEXIT_STOP.contains(d)).isPresent();
 
-            final boolean semaStop = !(wing2.filter(wing -> (Boolean) wing).isPresent())
-                    && !(wing1.filter(s -> (Boolean) s).isPresent());
+            final boolean hlmain40 = info.next.getProperty(SignalHL.STOPSIGNAL)
+                    .filter(c -> Signallists.HL_40_MAIN.contains(c)).isPresent()
+                    || info.next.getProperty(SignalHL.EXITSIGNAL).filter(HLExit.HL2_3::equals)
+                            .isPresent();
+
+            final boolean nextSema = semaType.filter(SemaType.MAIN::equals).isPresent()
+                    || semaType.filter(SemaType.MAIN_SMALL::equals).isPresent();
+
+            final boolean semaStop = !(wing1.filter(wing -> (Boolean) wing).isPresent()
+                    || (wing2.filter(s -> (Boolean) s).isPresent())) && nextSema;
 
             final boolean semaHP2 = wing1.filter(wing -> (Boolean) wing).isPresent()
-                    && wing2.filter(wing -> (Boolean) wing).isPresent();
+                    && wing2.filter(wing -> (Boolean) wing).isPresent() && nextSema;
+
+            final boolean hp2 = nextHP.filter(HP.HP2::equals).isPresent()
+                    || nextHPHOME.filter(HPHome.HP2::equals).isPresent();
+
+            final boolean hlSlowDown = (hlmain40
+                    && !getlightbar.filter(HLLightbar.GREEN::equals).isPresent());
+
+            final boolean ksSlowdown = ksStopsignal.filter(Signallists.KS_GO::contains).isPresent()
+                    && ksZS3.filter(zs -> zs.compareTo(ZS32.Z8) < 0 && zs.compareTo(ZS32.Z1) >= 0)
+                            .isPresent();
+
+            final boolean anySlowdown = hp2 || hlSlowDown || ksSlowdown;
 
             final boolean hvStop = hvStop1 || hvStop2;
-
-            checkStop(ksStop, values);
-            checkStop(hlStop, values);
-            checkStop(hvStop, values);
 
             checkSpeed(nexthlZS3PLATE, values);
             checkSpeed(hvZS3, values);
@@ -145,15 +163,16 @@ public final class SemaphoreConfig implements ISignalAutoconfig {
 
             values.put(SignalSemaphore.SEMA_VR, SemaDist.VR1);
 
-            if (semaHP2) {
-                values.put(SignalSemaphore.SEMA_VR, SemaDist.VR2);
-            }
-
             final boolean anyStop = hlStop || semaStop || hvStop;
 
-            if (anyStop) {
+            if (anyStop || ksStop) {
 
                 values.put(SignalSemaphore.SEMA_VR, SemaDist.VR0);
+            }
+
+            if (anySlowdown || semaHP2) {
+
+                values.put(SignalSemaphore.SEMA_VR, SemaDist.VR2);
             }
 
         } else {
