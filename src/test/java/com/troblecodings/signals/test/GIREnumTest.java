d11d0369f7f465f681ab5438ecc21d80b6efdd9e
MrTroble <nifroh56@gmail.com>
2022-12-03 16:02:04 +0100

    fix: test runner for enum constants

--- src/test/java/com/troblecodings/signals/test/GIREnumTest.java
+++ src/test/java/com/troblecodings/signals/test/GIREnumTest.java
@@ -18,9 +18,9 @@ public class GIREnumTest {
     @Test
     public void testEnum() {
         final Map<String, JsonEnum> map = JsonEnum.PROPERTIES;
-        assertTrue(map.containsKey("ZS32"));
-        assertTrue(map.containsKey("BUE"));
-        final String[] array = map.get("BUE").getAllowedValues().toArray(new String[0]);
+        assertTrue(map.containsKey("zs32"));
+        assertTrue(map.containsKey("bue"));
+        final String[] array = map.get("bue").getAllowedValues().toArray(new String[0]);
         assertArrayEquals(array, new String[] {
                 "BUE2_1", "BUE2_2", "BUE2_3", "BUE2_4", "BUE3", "BUE4", "BUE5"
         });
