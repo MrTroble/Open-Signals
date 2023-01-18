d1dbd0b8525075c5e1a06f0ddcb06cc2f712af6e
Nico Froehlich <nicofroehlich@icloud.com>
2023-01-18 23:12:23 +0100

    rm: old placement tool code

--- src/main/java/com/troblecodings/signals/items/Placementtool.java
+++ src/main/java/com/troblecodings/signals/items/Placementtool.java
@@ -37,7 +37,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class Placementtool extends BlockItem
+public class Placementtool extends Item
         implements IIntegerable<Signal>, ITagableItem, MessageWrapper {
 
     public static final String BLOCK_TYPE_ID = "blocktypeid";
@@ -46,129 +46,7 @@ public class Placementtool extends BlockItem
     public final ArrayList<Signal> signals = new ArrayList<>();
 
     public Placementtool() {
-        super(null, new Item.Properties().tab(OSTabs.TAB));
-    }
-
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level worldIn, Player player,
-            InteractionHand hand) {
-        if (!worldIn.isClientSide) {
-            OpenSignalsMain.handler.invokeGui(Placementtool.class, player, worldIn,
-                    player.getOnPos(), "placementtool");
-        }
-        return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand),
-                worldIn.isClientSide);
-    }
-
-    @Override
-    public InteractionResult useOn(UseOnContext context) {
-        final Player player = context.getPlayer();
-        final Level worldIn = context.getLevel();
-        if (player.isShiftKeyDown()) {
-            if (!worldIn.isClientSide) {
-                OpenSignalsMain.handler.invokeGui(Placementtool.class, player, worldIn,
-                        player.getOnPos(), "placementtool");
-            }
-            return InteractionResult.sidedSuccess(worldIn.isClientSide);
-        }
-        return super.useOn(context);
-    }
-
-    @Override
-    public InteractionResult place(final BlockPlaceContext context) {
-        final InteractionResult result = super.place(context);
-        final Player player = context.getPlayer();
-        final Level worldIn = context.getLevel();
-        final NBTWrapper compound = new NBTWrapper(player.getMainHandItem().getOrCreateTag());
-        final BlockPos pos = context.getClickedPos();
-        if (!compound.contains(BLOCK_TYPE_ID)) {
-            if (!worldIn.isClientSide)
-                return result;
-            translateMessageWrapper(player, "pt.itemnotset");
-            return InteractionResult.FAIL;
-        }
-        if (player.isShiftKeyDown()) {
-            if (worldIn.isClientSide())
-                return result;
-            OpenSignalsMain.handler.invokeGui(Placementtool.class, player, worldIn,
-                    player.getOnPos(), "placementtool");
-            return result;
-        }
-
-        final Signal signal = getObjFromID(compound.getInteger(BLOCK_TYPE_ID));
-        final Map<SEProperty, String> states = new HashMap<>();
-        final List<SEProperty> properties = signal.getProperties();
-
-        properties.forEach(property -> {
-            if (property.isChangabelAtStage(ChangeableStage.APISTAGE_NONE_CONFIG)) {
-                states.put(property, property.getDefault());
-                return;
-            }
-            if (!compound.contains(property.getName())) {
-                return;
-            }
-            if (property.isChangabelAtStage(ChangeableStage.GUISTAGE)) {
-                states.put(property,
-                        property.getObjFromID(compound.getInteger(property.getName())));
-            } else if (property.isChangabelAtStage(ChangeableStage.APISTAGE)
-                    && compound.getInteger(property.getName()) == 1) {
-                states.put(property, property.getDefault());
-            }
-        });
-        final SignalTileEntity tile = new SignalTileEntity(
-                new TileEntityInfo(pos, signal.defaultBlockState()));
-        final int height = signal.getHeight(states);
-        BlockPos lastPos = context.getClickedPos();
-        for (int i = 0; i < height; i++) {
-            if (!worldIn.isEmptyBlock(lastPos = lastPos.above())) {
-                worldIn.removeBlock(lastPos, true);
-                return InteractionResult.FAIL;
-            }
-        }
-        lastPos = context.getClickedPos();
-        for (int i = 0; i < height; i++) {
-            worldIn.setBlockAndUpdate(lastPos = lastPos.above(),
-                    OSBlocks.GHOST_BLOCK.getStateForPlacement(context));
-        }
-
-        final String name = compound.getString(SIGNAL_CUSTOMNAME);
-        if (name != null && tile != null) {
-            tile.setNameWithOutSync(name);
-            states.put(Signal.CUSTOMNAME, name);
-        }
-        worldIn.setBlockAndUpdate(pos, signal.getStateForPlacement(context));
-        worldIn.setBlockEntity(tile);
-        SignalStateHandler.setStates(new SignalStateInfo(worldIn, pos), states);
-        return InteractionResult.SUCCESS;
-    }
-
-    @Override
-    public Block getBlock() {
-        return OSBlocks.GHOST_BLOCK;
-    }
-
-    @Override
-    public void registerBlocks(Map<Block, Item> map, Item item) {
-    }
-
-    @Override
-    public void removeFromBlockToItemMap(Map<Block, Item> blockToItemMap, Item itemIn) {
-    }
-
-    @Override
-    public String getDescriptionId() {
-        return this.getOrCreateDescriptionId();
-    }
-
-    @Override
-    protected BlockState getPlacementState(BlockPlaceContext context) {
-        final Player player = context.getPlayer();
-        final NBTWrapper compound = new NBTWrapper(player.getMainHandItem().getOrCreateTag());
-        if (!compound.contains(BLOCK_TYPE_ID)) {
-            translateMessageWrapper(player, "pt.itemnotset");
-            return null;
-        }
-        return this.signals.get(compound.getInteger(BLOCK_TYPE_ID)).getStateForPlacement(context);
+        super(new Item.Properties().tab(OSTabs.TAB));
     }
 
     @OnlyIn(Dist.CLIENT)
