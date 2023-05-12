efce162d9b6ac48bc65ebcd0fa927935abebb5e5
Philipp <philippdal@gmail.com>
2023-05-12 18:04:33 +0200

    fix: pole block collision

--- src/main/java/com/troblecodings/signals/blocks/Post.java
+++ src/main/java/com/troblecodings/signals/blocks/Post.java
@@ -1,16 +1,16 @@
 package com.troblecodings.signals.blocks;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class Post extends BasicBlock {
 
-    private static final VoxelShape BOUNDING_BOX = Block.box(7 * 0.0625, 0.0, 7 * 0.0625,
+    private static final VoxelShape BOUNDING_BOX = VoxelShapes.box(7 * 0.0625, 0.0, 7 * 0.0625,
             9 * 0.0625, 16 * 0.0625, 9 * 0.0625);
 
     public Post() {
