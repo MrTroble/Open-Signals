f7cb17b55cae0c306d079b17cff8ab66ae5d5eff
Cedric <cedric.hathaway05@gmail.com>
2023-05-14 11:44:32 +0200

    more porting fixes

--- src/main/java/com/troblecodings/signals/core/RenderOverlayInfo.java
+++ src/main/java/com/troblecodings/signals/core/RenderOverlayInfo.java
@@ -1,22 +1,19 @@
 package com.troblecodings.signals.core;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
 import com.troblecodings.signals.tileentitys.SignalTileEntity;
 
 import net.minecraft.client.gui.FontRenderer;
 
 public class RenderOverlayInfo {
-    public final MatrixStack stack;
     public final double x;
     public final double y;
     public final double z;
     public SignalTileEntity tileEntity;
     public final FontRenderer font;
 
-    public RenderOverlayInfo(final MatrixStack stack, final double x, final double y,
-            final double z, final FontRenderer fontRenderer) {
+    public RenderOverlayInfo(final double x, final double y, final double z,
+            final FontRenderer fontRenderer) {
         super();
-        this.stack = stack;
         this.x = x;
         this.y = y;
         this.z = z;
