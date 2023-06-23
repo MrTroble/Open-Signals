737f684da550b80f1e8457ba0bc9e09540a5bcb2
Philipp <philippdal@gmail.com>
2023-06-23 12:37:51 +0200

    fix: startup issue

--- src/main/java/com/troblecodings/signals/core/SignalAngel.java
+++ src/main/java/com/troblecodings/signals/core/SignalAngel.java
@@ -4,7 +4,9 @@ import org.lwjgl.util.vector.Quaternion;
 
 import com.troblecodings.core.interfaces.NamableWrapper;
 
-public enum SignalAngel implements NamableWrapper {
+import net.minecraft.util.IStringSerializable;
+
+public enum SignalAngel implements IStringSerializable, NamableWrapper {
 
     ANGEL0, ANGEL22P5, ANGEL45, ANGEL67P5, ANGEL90, ANGEL112P5, ANGEL135, ANGEL157P5, ANGEL180,
     ANGEL202P5, ANGEL225, ANGEL247P5, ANGEL270, ANGEL292P5, ANGEL315, ANGEL337P5;
@@ -26,9 +28,14 @@ public enum SignalAngel implements NamableWrapper {
     public float getDregree() {
         return this.ordinal() * 22.5f;
     }
+    
+    @Override
+    public String getName() {
+        return this.name().toLowerCase();
+    }
 
     @Override
     public String getNameWrapper() {
-        return this.name().toLowerCase();
+        return getName();
     }
 }
