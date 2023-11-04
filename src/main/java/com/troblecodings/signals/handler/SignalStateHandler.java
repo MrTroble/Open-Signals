b7faca340161508ee3ad72c81dcf3da11fcb759a
Cedric <cedric.hathaway05@gmail.com>
2023-11-04 14:08:33 +0100

    added more debug output

--- src/main/java/com/troblecodings/signals/handler/SignalStateHandler.java
+++ src/main/java/com/troblecodings/signals/handler/SignalStateHandler.java
@@ -449,6 +449,7 @@ public final class SignalStateHandler implements INetworkSync {
                     sendToPlayer(info.info, properties, player);
                 }
                 updateListeners(info.info, properties, ChangedState.ADDED_TO_CACHE);
+                System.out.println("Loaded [" + info.info + "]!");
             });
         });
     }
@@ -478,6 +479,7 @@ public final class SignalStateHandler implements INetworkSync {
                     return;
                 createToFile(info.info, properties);
                 updateListeners(info.info, properties, ChangedState.REMOVED_FROM_CACHE);
+                System.out.println("Unloaded [" + info.info + "]!");
             });
         });
     }
