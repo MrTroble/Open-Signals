4aab49d3403dc6731483604163fbcc5c08bb0b4e
Philipp <philippdal@gmail.com>
2023-12-31 13:24:33 +0100

    fix: Signal angle server crash

--- src/main/java/com/troblecodings/signals/core/SignalAngel.java
+++ src/main/java/com/troblecodings/signals/core/SignalAngel.java
@@ -4,24 +4,24 @@ import com.troblecodings.core.QuaternionWrapper;
 import com.troblecodings.core.interfaces.NamableWrapper;
 
 import net.minecraft.util.math.vector.Quaternion;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public enum SignalAngel implements NamableWrapper {
 
     ANGEL0, ANGEL22P5, ANGEL45, ANGEL67P5, ANGEL90, ANGEL112P5, ANGEL135, ANGEL157P5, ANGEL180,
     ANGEL202P5, ANGEL225, ANGEL247P5, ANGEL270, ANGEL292P5, ANGEL315, ANGEL337P5;
 
-    private Quaternion quaternion;
-
     private SignalAngel() {
-        quaternion = QuaternionWrapper.fromXYZ(0, (float) getRadians(), 0);
     }
 
     public double getRadians() {
-        return Math.PI * 2.0 - (this.ordinal() / 16.0) * Math.PI * 2.0;
+        return (this.ordinal() / 16.0) * Math.PI * 2.0;
     }
 
+    @OnlyIn(Dist.CLIENT)
     public Quaternion getQuaternion() {
-        return this.quaternion;
+        return QuaternionWrapper.fromXYZ(0, (float) getRadians(), 0);
     }
 
     @Override
