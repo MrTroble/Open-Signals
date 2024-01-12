0f30acdc40feeb06db91bf20a0f291eddc0b2492
Cedric <cedric.hathaway05@gmail.com>
2024-01-12 14:53:41 +0100

    added system to destroy a signal bridge with one click

--- src/main/java/com/troblecodings/signalbridge/SignalBridgeBasicBlock.java
+++ src/main/java/com/troblecodings/signalbridge/SignalBridgeBasicBlock.java
@@ -5,7 +5,10 @@ import com.troblecodings.signals.config.ConfigHandler;
 import com.troblecodings.signals.enums.SignalBridgeType;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -30,13 +33,31 @@ public class SignalBridgeBasicBlock extends BasicBlock {
 
     @Override
     public boolean shouldHaveItem() {
-        return false;
+        // true for debugging
+        return true;
     }
 
     @Override
     public VoxelShape getShape(final BlockState state, final BlockGetter getter, final BlockPos pos,
             final CollisionContext context) {
         return Shapes.block();
+    }
+
+    @Override
+    public void destroy(final LevelAccessor acess, final BlockPos pos, final BlockState state) {
+        for (final Direction direction : Direction.values()) {
+            checkAndDestroyBlockInDirection(acess, pos, state, direction);
+        }
+    }
+
+    private void checkAndDestroyBlockInDirection(final LevelAccessor acess, final BlockPos basePos,
+            final BlockState baseState, final Direction direction) {
+        final BlockPos thisPos = basePos.relative(direction);
+        final Block otherBlock = acess.getBlockState(thisPos).getBlock();
+        if (otherBlock instanceof SignalBridgeBasicBlock) {
+            acess.destroyBlock(thisPos, false);
+            otherBlock.destroy(acess, thisPos, baseState);
+        }
     }
 
 }
