e3a5d0d06f1a36d19c6f5c0208e73a46604f758e
Philipp <philippdal@gmail.com>
2024-04-02 16:59:12 +0200

    ref: checkstyle class final modifier

--- src/main/java/com/troblecodings/signals/core/DestroyHelper.java
+++ src/main/java/com/troblecodings/signals/core/DestroyHelper.java
@@ -8,7 +8,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class DestroyHelper {
+public final class DestroyHelper {
 
     private DestroyHelper() {
     }
