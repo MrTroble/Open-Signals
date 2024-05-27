c104f36d004690740e900f71238da2b8456c6e6e
Cedric <cedric.hathaway05@gmail.com>
2024-05-27 14:04:11 +0200

    fix: resetting of signals after stop

--- src/main/java/com/troblecodings/signals/signalbox/SignalBoxPathway.java
+++ src/main/java/com/troblecodings/signals/signalbox/SignalBoxPathway.java
@@ -361,6 +361,7 @@ public class SignalBoxPathway implements IChunkLoadable, SignalStateListener {
         unregisterSignalUpdater();
         resetFirstSignal();
         resetOther();
+        isBlocked = true;
     }
 
     private void resetFirstSignal() {
