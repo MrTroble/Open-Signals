4d8c864bdf022ab6af3f4488791d17d5f34584e3
Philipp <philippdal@gmail.com>
2025-12-29 15:34:02 +0100

    ref: merge conflicts

--- src/main/java/com/troblecodings/signals/guis/GuiSignalController.java
+++ src/main/java/com/troblecodings/signals/guis/GuiSignalController.java
@@ -24,6 +24,7 @@ import com.troblecodings.guilib.ecs.interfaces.IIntegerable;
 import com.troblecodings.signals.OpenSignalsMain;
 import com.troblecodings.signals.SEProperty;
 import com.troblecodings.signals.blocks.Signal;
+import com.troblecodings.signals.core.JsonEnum;
 import com.troblecodings.signals.core.StateInfo;
 import com.troblecodings.signals.enums.EnumMode;
 import com.troblecodings.signals.enums.EnumState;
