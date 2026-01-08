64efe8dc24be3e6f157157c595ebe919949900a9
Cedric <cedric.hathaway05@gmail.com>
2026-01-08 19:41:30 +0100

    ref: checkstyle

--- src/main/java/com/troblecodings/signals/animation/SignalAnimationHandler.java
+++ src/main/java/com/troblecodings/signals/animation/SignalAnimationHandler.java
@@ -52,8 +52,8 @@ public class SignalAnimationHandler {
         this.tile = tile;
     }
 
-    private final Map<Entry<IBakedModel, BufferBuilder>, Entry<ModelTranslation, List<SignalAnimation>>> //
-    animationPerModel = new HashMap<>();
+    private final Map<Entry<IBakedModel, BufferBuilder>, Entry<ModelTranslation, List<SignalAnimation>>> animationPerModel = //
+            new HashMap<>();
 
     public void render(final RenderAnimationInfo info) {
         final World world = tile.getWorld();
