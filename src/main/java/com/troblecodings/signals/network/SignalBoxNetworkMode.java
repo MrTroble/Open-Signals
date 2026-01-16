551cb1efbddf8b846f5c02446f1c138c347be0f7
Cedric <cedric.hathaway05@gmail.com>
2026-01-16 13:58:15 +0100

    feat: first try for new networking

---
+++ src/main/java/com/troblecodings/signals/network/SignalBoxNetworkMode.java
@@ -0,0 +1,53 @@
+package com.troblecodings.signals.network;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import com.troblecodings.core.ReadBuffer;
+import com.troblecodings.core.WriteBuffer;
+
+public class SignalBoxNetworkMode {
+
+    private static final List<SignalBoxNetworkMode> NETWORK_ENTRIES = new ArrayList<>();
+
+    private int id;
+    private final Consumer<ReadBuffer> read;
+
+    public SignalBoxNetworkMode(final Consumer<ReadBuffer> read) {
+        this.read = read;
+        this.id = NETWORK_ENTRIES.size();
+        NETWORK_ENTRIES.add(this);
+    }
+
+    public static SignalBoxNetworkMode getModeFromBuffer(final ReadBuffer buffer) {
+        return NETWORK_ENTRIES.get(buffer.getInt());
+    }
+
+    public void executeRead(final ReadBuffer buffer) {
+        read.accept(buffer);
+    }
+
+    public WriteBuffer getBuffer() {
+        final WriteBuffer buffer = new WriteBuffer();
+        buffer.putInt(id);
+        return buffer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, read);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || (getClass() != obj.getClass()))
+            return false;
+        final SignalBoxNetworkMode other = (SignalBoxNetworkMode) obj;
+        return id == other.id && Objects.equals(read, other.read);
+    }
+
+}
--- src/main/java/com/troblecodings/signals/signalbox/entrys/PathOptionEntry.java
+++ src/main/java/com/troblecodings/signals/signalbox/entrys/PathOptionEntry.java
@@ -36,6 +36,14 @@ public class PathOptionEntry implements INetworkSaveable, ISaveable {
         pathEntry.setValue(value);
     }
 
+    public void addEntry(final PathEntryType<?> entryType, final IPathEntry<?> entry) {
+        if (entryType == null) {
+            pathEntrys.remove(entryType);
+            return;
+        }
+        pathEntrys.put(entryType, entry);
+    }
+
     public void removeEntry(final PathEntryType<?> type) {
         pathEntrys.remove(type);
     }
