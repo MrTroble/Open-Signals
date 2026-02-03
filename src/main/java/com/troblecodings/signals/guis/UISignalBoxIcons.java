4d50be58919ea67f07faf49c08f700590670f3b6
Cedric <cedric.hathaway05@gmail.com>
2026-02-03 17:04:00 +0100

    fix: issues with loading of signalbox

--- src/main/java/com/troblecodings/signals/guis/UISignalBoxIcons.java
+++ src/main/java/com/troblecodings/signals/guis/UISignalBoxIcons.java
@@ -3,14 +3,9 @@ package com.troblecodings.signals.guis;
 import com.troblecodings.signals.OpenSignalsMain;
 
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-
-@OnlyIn(Dist.CLIENT)
 public final class UISignalBoxIcons {
 
     private UISignalBoxIcons() {
-
     }
 
     public static final ResourceLocation ICON =
