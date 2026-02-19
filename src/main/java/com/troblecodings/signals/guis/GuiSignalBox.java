57ae961178245f3baf85ae0240b63a86041a1423
Cedric <cedric.hathaway05@gmail.com>
2026-02-19 15:11:55 +0100

    fix: pathway can't be set with activated subsidiary

--- src/main/java/com/troblecodings/signals/guis/GuiSignalBox.java
+++ src/main/java/com/troblecodings/signals/guis/GuiSignalBox.java
@@ -275,6 +275,13 @@ public class GuiSignalBox extends GuiBase {
 
     private void checkForMultiplePathTypes(final SignalBoxNode start, final SignalBoxNode end) {
         final List<PathType> possibleTypes = start.getPossibleTypes(end);
+        for (final ModeSet mode : start.getModes().keySet()) {
+            if (start.getSubsidiaryState(mode) != null) {
+                infoUpdate(I18Wrapper
+                        .format("error." + PathwayRequestMode.SUBISIDIARY_ENABLED.getName()));
+                return;
+            }
+        }
         if (possibleTypes.isEmpty()) {
             infoUpdate(
                     I18Wrapper.format("error." + PathwayRequestMode.NO_EQUAL_PATH_TYPE.getName()));
@@ -703,6 +710,7 @@ public class GuiSignalBox extends GuiBase {
             });
             updateSignalState(node);
         });
+        enabledSubsidiaries.clear();
         container.enabledSubsidiaryTypes.clear();
     }
 
