66521665de64d49d6243751c5cc035794fb15b3f
Cedric <cedric.hathaway05@gmail.com>
2026-02-23 13:29:10 +0100

    fix: issue with 2 players in signal box

--- src/main/java/com/troblecodings/signals/guis/ContainerSignalBox.java
+++ src/main/java/com/troblecodings/signals/guis/ContainerSignalBox.java
@@ -59,6 +59,7 @@ public class ContainerSignalBox extends ContainerBase implements UIClientSync, I
 
     private final Map<BlockPos, LinkType> posForType = new HashMap<>();
     private SignalBoxNetworkHandler network = new SignalBoxNetworkHandler();
+    private EntityPlayer player;
 
     protected Consumer<SignalBoxNode> updateSignalState = (node) -> {
     };
@@ -299,8 +300,8 @@ public class ContainerSignalBox extends ContainerBase implements UIClientSync, I
     public boolean canInteractWith(final EntityPlayer playerIn) {
         if (tile.isBlocked() && !tile.isValid(playerIn))
             return false;
-        if (this.info.player == null) {
-            this.info.player = playerIn;
+        if (this.player == null) {
+            this.player = playerIn;
             this.tile.add(this);
         }
         return true;
