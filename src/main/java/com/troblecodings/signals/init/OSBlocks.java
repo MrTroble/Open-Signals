6f6484e7146e97afb47b238d0840fbaa3a4635f6
Cedric <103828586+Uhutown@users.noreply.github.com>
2026-05-21 13:42:17 +0200

    feat: added signal reader block

--- src/main/java/com/troblecodings/signals/init/OSBlocks.java
+++ src/main/java/com/troblecodings/signals/init/OSBlocks.java
@@ -17,6 +17,7 @@ import com.troblecodings.signals.blocks.RedstoneInput;
 import com.troblecodings.signals.blocks.Signal;
 import com.troblecodings.signals.blocks.SignalBox;
 import com.troblecodings.signals.blocks.SignalController;
+import com.troblecodings.signals.blocks.SignalReader;
 import com.troblecodings.signals.blocks.TrainNumberBlock;
 import com.troblecodings.signals.contentpacks.MonitorBlockParser;
 import com.troblecodings.signals.contentpacks.SignalBridgeBlockParser;
@@ -47,6 +48,7 @@ public final class OSBlocks {
     public static final PathwayRequester PATHWAY_REQUESTER = new PathwayRequester();
     public static final TrainNumberBlock TRAIN_NUMBER_BLOCK = new TrainNumberBlock();
     public static final PostConnectable POST_CONNECTABLE = new PostConnectable();
+    public static final SignalReader SIGNALREADER = new SignalReader();
 
     public static final List<BasicBlock> BLOCKS_TO_REGISTER = new ArrayList<>();
 
