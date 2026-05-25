39e5e6ae026f2b86ce5eb4c841a426637bac48f6
Cedric <103828586+Uhutown@users.noreply.github.com>
2026-05-25 22:19:39 +0200

    ref: redone logic of signal reader

--- src/main/java/com/troblecodings/signals/guis/ContainerSignalReader.java
+++ src/main/java/com/troblecodings/signals/guis/ContainerSignalReader.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import com.google.common.collect.Maps;
 import com.troblecodings.core.ReadBuffer;
 import com.troblecodings.core.WriteBuffer;
 import com.troblecodings.guilib.ecs.ContainerBase;
@@ -16,6 +18,7 @@ import com.troblecodings.signals.core.NetworkBufferWrappers;
 import com.troblecodings.signals.enums.ChangeableStage;
 import com.troblecodings.signals.handler.SignalStateHandler;
 import com.troblecodings.signals.handler.SignalStateInfo;
+import com.troblecodings.signals.parser.interm.LogicalSymbols;
 import com.troblecodings.signals.tileentitys.SignalReaderTileEntity;
 
 import net.minecraft.core.BlockPos;
@@ -27,7 +30,8 @@ public class ContainerSignalReader extends ContainerBase {
         super(info);
     }
 
-    protected final Map<Direction, Map<SEProperty, String>> statesForFace = new HashMap<>();
+    protected final Map<Direction, Map.Entry<LogicalSymbols[], Map.Entry<SEProperty, String>[]>> statesForFace =
+            new HashMap<>();
     protected final List<SEProperty> selectableProperties = new ArrayList<>();
     protected Signal signal;
     protected BlockPos pos;
@@ -55,9 +59,7 @@ public class ContainerSignalReader extends ContainerBase {
         buffer.putInt(signal.getID());
         buffer.putBlockPos(tile.getLinkedSignalPos());
         buffer.putMap(tile.getPropertiesForFace(), WriteBuffer.getEnumConsumer(),
-                (propBuf, states) -> propBuf.putMap(states,
-                        NetworkBufferWrappers.getSEPropertyConsumer(signal),
-                        WriteBuffer.STRING_CONSUMER));
+                (buf, mainEntry) -> serializeEntries(mainEntry, buf));
         buffer.putList(selectableProperties, NetworkBufferWrappers.getSEPropertyConsumer(signal));
         OpenSignalsMain.network.sendTo(getPlayer(), buffer);
     }
@@ -68,9 +70,7 @@ public class ContainerSignalReader extends ContainerBase {
         selectableProperties.clear();
         signal = Signal.getSignalByID(buf.getInt());
         pos = buf.getBlockPos();
-        buf.getMap(ReadBuffer.getEnumFunction(Direction.class),
-                mapBuf -> mapBuf.getMap(NetworkBufferWrappers.getSEPropertyFunc(signal),
-                        ReadBuffer.STRING_FUNCTION))
+        buf.getMap(ReadBuffer.getEnumFunction(Direction.class), buffer -> deserializeEntry(buffer))
                 .forEach(statesForFace::put);
         buf.getList(NetworkBufferWrappers.getSEPropertyFunc(signal))
                 .forEach(selectableProperties::add);
@@ -80,23 +80,75 @@ public class ContainerSignalReader extends ContainerBase {
     @Override
     public void deserializeServer(final ReadBuffer buf) {
         final Direction dir = buf.getEnumValue(Direction.class);
-        final SEProperty property = signal.getPropertyByIndex(buf.getInt());
-        final int valueID = buf.getInt();
-        if (valueID == -1) {
-            tile.removePropertyFromDirection(dir, property);
+
+        final Entry<LogicalSymbols[], Entry<SEProperty, String>[]> entry = deserializeEntry(buf);
+
+        if (isArrayEmpty(entry.getKey()) && isArrayEmpty(entry.getValue())) {
+            tile.removeDirection(dir);
         } else {
-            tile.addPropertyToDirection(dir, property, property.getObjFromID(valueID));
+            tile.setUpForDirection(dir, Maps.immutableEntry(entry.getKey(), entry.getValue()));
         }
         tile.setChanged();
     }
 
-    protected void sendPropertyToServer(final Direction dir, final SEProperty property,
-            final int value) {
+    protected void sendToServerForDirection(final Direction dir) {
+        final Entry<LogicalSymbols[], Entry<SEProperty, String>[]> entries = statesForFace.get(dir);
+        if (entries == null)
+            return;
         final WriteBuffer buffer = new WriteBuffer();
         buffer.putEnumValue(dir);
-        buffer.putInt(signal.getIDFromProperty(property));
-        buffer.putInt(value);
+        serializeEntries(entries, buffer);
         OpenSignalsMain.network.sendTo(getPlayer(), buffer);
+    }
+
+    private void serializeEntries(final Entry<LogicalSymbols[], Entry<SEProperty, String>[]> entry,
+            final WriteBuffer buffer) {
+        buffer.putInt(entry.getKey().length);
+        for (final LogicalSymbols symbol : entry.getKey()) {
+            buffer.putInt(symbol != null ? symbol.ordinal() : -1);
+        }
+        buffer.putInt(entry.getValue().length);
+        for (final Entry<SEProperty, String> property : entry.getValue()) {
+            if (property != null) {
+                buffer.putInt(signal.getIDFromProperty(property.getKey()));
+                buffer.putInt(property.getKey().getParent().getIDFromValue(property.getValue()));
+            } else {
+                buffer.putInt(-1);
+            }
+        }
+    }
+
+    private Entry<LogicalSymbols[], Entry<SEProperty, String>[]> deserializeEntry(
+            final ReadBuffer buffer) {
+        final int logicalSymbolLength = buffer.getInt();
+        final LogicalSymbols[] symbols = new LogicalSymbols[9];
+        for (int i = 0; i < logicalSymbolLength; i++) {
+            final int logicalSymbol = buffer.getInt();
+            if (logicalSymbol != -1) {
+                symbols[i] = LogicalSymbols.values()[logicalSymbol];
+            }
+        }
+
+        final int propertiesLength = buffer.getInt();
+        @SuppressWarnings("unchecked")
+        final Entry<SEProperty, String>[] propertyEntries = new Entry[10];
+        for (int i = 0; i < propertiesLength; i++) {
+            final int propertyID = buffer.getInt();
+            if (propertyID != -1) {
+                final SEProperty property = signal.getPropertyByIndex(propertyID);
+                final String value = property.getObjFromID(buffer.getInt());
+                propertyEntries[i] = Maps.immutableEntry(property, value);
+            }
+        }
+        return Maps.immutableEntry(symbols, propertyEntries);
+    }
+
+    private static boolean isArrayEmpty(final Object[] array) {
+        for (final Object obj : array) {
+            if (obj != null)
+                return false;
+        }
+        return true;
     }
 
 }
