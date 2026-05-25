611250f997053bacdc3ec2a8bd6bd8eb8f5f2f39
Cedric <103828586+Uhutown@users.noreply.github.com>
2026-05-25 13:06:47 +0200

    fix: issues with side side selection of RS profiles in SignalController

--- src/main/java/com/troblecodings/signals/guis/ContainerSignalController.java
+++ src/main/java/com/troblecodings/signals/guis/ContainerSignalController.java
@@ -189,8 +189,8 @@ public class ContainerSignalController extends ContainerBase
                 break;
             }
             case SET_PROFILE: {
-                final EnumState state = buffer.getEnumValue(EnumState.class);
                 final Direction direction = buffer.getEnumValue(Direction.class);
+                final EnumState state = buffer.getEnumValue(EnumState.class);
                 final int profile = buffer.getByteToUnsignedInt();
                 if (profile == -1) {
                     controllerEntity.removeProfileFromDirection(direction, state);
