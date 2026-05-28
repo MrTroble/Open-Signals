09b6d6b27627a0a418937248d9a049c57a58bf28
Cedric <cedric.hathaway05@gmail.com>
2026-05-28 17:55:15 +0200

    fix: closing Info & Help page at beginnging

--- src/main/java/com/troblecodings/signals/guis/GuiSignalBox.java
+++ src/main/java/com/troblecodings/signals/guis/GuiSignalBox.java
@@ -238,7 +238,6 @@ public class GuiSignalBox extends GuiBase {
             }
             return;
         }
-        this.helpPage.setShowHelpPage(false);
         rendering.clearSelection();
         if (mouse != MouseEvent.LEFT_MOUSE)
             return;
