c94e6e41f0718a60ab76cee2d8d1cb3e7a400d8d
Cedric <cedric.hathaway05@gmail.com>
2026-06-09 19:15:57 +0200

    fix: insets of monitorblock

--- src/main/java/com/troblecodings/signals/blocks/MonitorTEBlock.java
+++ src/main/java/com/troblecodings/signals/blocks/MonitorTEBlock.java
@@ -57,7 +57,7 @@ public class MonitorTEBlock extends Monitor {
         final float renderSizeY = tile.getRenderEnd().getY() - tile.getRenderStart().getY() + 1f;
 
         final float insets = getMonitorProperties().getInsets();
-        final float colorInsets = insets / STEPS_PER_BLOCK;
+        final float colorInsets = (insets - 6) / STEPS_PER_BLOCK;
 
         final DrawInfo drawInfo = new DrawInfo(0, 0, info.tick);
         drawInfo.push();
