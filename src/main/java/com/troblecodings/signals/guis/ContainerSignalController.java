178ded5e590c31d3f1a0abb37b5051806d796697
Cedric <cedric.hathaway05@gmail.com>
2026-06-09 19:04:07 +0200

    fix: wrong disabled IDs in signal controller

--- src/main/java/com/troblecodings/signals/guis/ContainerSignalController.java
+++ src/main/java/com/troblecodings/signals/guis/ContainerSignalController.java
@@ -192,7 +192,7 @@ public class ContainerSignalController extends ContainerBase
             case SET_PROFILE: {
                 final EnumFacing direction = buffer.getEnumValue(EnumFacing.class);
                 final EnumState state = buffer.getEnumValue(EnumState.class);
-                final int profile = buffer.getByteToUnsignedInt();
+                final int profile = buffer.getInt();
                 if (profile == -1) {
                     controllerEntity.removeProfileFromDirection(direction, state);
                 } else {
@@ -234,7 +234,7 @@ public class ContainerSignalController extends ContainerBase
         buffer.putEnumValue(SignalControllerNetwork.SET_PROFILE);
         buffer.putEnumValue(facing);
         buffer.putEnumValue(state);
-        buffer.putByte((byte) profile);
+        buffer.putInt(profile);
         OpenSignalsMain.network.sendTo(info.player, buffer);
     }
 
